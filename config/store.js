3ed24de3a1e6ed2b8dace49f6cf613fb78612229
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-19 15:35:45 +0200

    Add support of Redux Dev Tool chrome extension

--- config/store.js
+++ config/store.js
@@ -1,6 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from '../src/reducers/index';
+
+const composeEnhancers = (typeof(window) !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 export default ( initialState ) =>
   createStore( rootReducer, initialState, composeEnhancers( applyMiddleware( thunkMiddleware ) ) );
